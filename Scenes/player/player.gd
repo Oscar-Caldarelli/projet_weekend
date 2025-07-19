74c6de4655847e70f7c197fa903b33caa105d4cd
Allex617 <allex617@gmail.com>
2025-07-19 18:10:05 +0200

    adding wall jump

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
+@onready var ray_cast_right: RayCast2D = $CharacterBody2D/RayCastRight
+@onready var ray_cast_left: RayCast2D = $CharacterBody2D/RayCastLeft
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const WALL_JUMP_VELOCITY_Y = -300.0
+const WALL_JUMP_VELOCITY_X = -200.0
 const DASH_SPEED = 900.0
 var dashing = false
 var canDash = true
@@ -14,8 +18,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_wall_only()):
+		if is_on_wall_only():
+			print(ray_cast_right)
+			print(ray_cast_left)
+			if ray_cast_right.is_colliding(): 
+				velocity.y = WALL_JUMP_VELOCITY_Y
+				velocity.x = WALL_JUMP_VELOCITY_X * -1
+			elif ray_cast_left.is_colliding() :
+				velocity.y = WALL_JUMP_VELOCITY_Y
+				velocity.x = WALL_JUMP_VELOCITY_X 
+		else : 
+			velocity.y = JUMP_VELOCITY
+			
 	if Input.is_action_just_pressed("dash") and canDash :
 		dashing = true
 		canDash = false
