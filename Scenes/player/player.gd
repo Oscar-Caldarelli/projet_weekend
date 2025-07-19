1199065c82c5b13f5c77bd0943ad35a0b02cd5a8
Allex617 <allex617@gmail.com>
2025-07-19 19:00:54 +0200

    Edit Air moving

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,12 +1,15 @@
-extends CharacterBody2D
+extends Node2D
 @onready var ray_cast_right: RayCast2D = $CharacterBody2D/RayCastRight
 @onready var ray_cast_left: RayCast2D = $CharacterBody2D/RayCastLeft
+@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
+@onready var dash_timer: Timer = $CharacterBody2D/dashTimer
+@onready var can_dash_timer: Timer = $CharacterBody2D/canDashTimer
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const WALL_JUMP_VELOCITY_Y = -300.0
-const WALL_JUMP_VELOCITY_X = -200.0
+const WALL_JUMP_VELOCITY_X = 1900.0
 const DASH_SPEED = 900.0
 var dashing = false
 var canDash = true
@@ -14,40 +17,43 @@ var canDash = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if not character_body_2d.is_on_floor():
+		character_body_2d.velocity += character_body_2d.get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_on_wall_only()):
-		if is_on_wall_only():
-			print(ray_cast_right)
-			print(ray_cast_left)
+	if Input.is_action_just_pressed("ui_accept") and (character_body_2d.is_on_floor() or character_body_2d.is_on_wall_only()):
+		if character_body_2d.is_on_wall_only():
+			print(ray_cast_right.is_colliding())
+			print(ray_cast_left.is_colliding())
 			if ray_cast_right.is_colliding(): 
-				velocity.y = WALL_JUMP_VELOCITY_Y
-				velocity.x = WALL_JUMP_VELOCITY_X * -1
+				character_body_2d.velocity.y = WALL_JUMP_VELOCITY_Y
+				character_body_2d.velocity.x = WALL_JUMP_VELOCITY_X * -1
 			elif ray_cast_left.is_colliding() :
-				velocity.y = WALL_JUMP_VELOCITY_Y
-				velocity.x = WALL_JUMP_VELOCITY_X 
+				character_body_2d.velocity.y = WALL_JUMP_VELOCITY_Y
+				character_body_2d.velocity.x = WALL_JUMP_VELOCITY_X 
 		else : 
-			velocity.y = JUMP_VELOCITY
+			character_body_2d.velocity.y = JUMP_VELOCITY
 			
 	if Input.is_action_just_pressed("dash") and canDash :
 		dashing = true
 		canDash = false
-		$dashTimer.start()
-		$canDashTimer.start()
+		dash_timer.start()
+		can_dash_timer.start()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		if dashing:
-			velocity.x = direction * DASH_SPEED
+			character_body_2d.velocity.x = direction * DASH_SPEED
 		else:
-			velocity.x = direction * SPEED
+			if character_body_2d.is_on_floor():
+				character_body_2d.velocity.x = direction * SPEED
+			else:
+				character_body_2d.velocity.x = move_toward(character_body_2d.velocity.x, direction * SPEED, SPEED * 0.1)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		character_body_2d.velocity.x = move_toward(character_body_2d.velocity.x, 0, SPEED)
+		
+	character_body_2d.move_and_slide()
 
 
 
